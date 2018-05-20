e494ce7a2413f72057cd91ce8c5c10b02c7befac
Mahmoud Bdeir <mahmoud.bdeir@gmail.com>
2018-05-20 10:56:13 +0300

    Added EventDriven programming example

---
+++ EventDriven/Car.cs
@@ -0,0 +1,27 @@
+﻿namespace MyApp
+{
+    public class Car
+    {
+        public delegate void Crash();
+        public event Crash OnCrash;
+
+        int speed;
+        public void SpeedUp()
+        {
+            speed++;
+            if (speed > 2)
+            {
+                OnCrash();
+            }
+        }
+        public void SlowDown()
+        {
+            speed--;
+        }
+
+        public override string ToString()
+        {
+            return speed.ToString();
+        }
+    }
+}
---
+++ EventDriven/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public partial class Form1 : Form
+    {
+        Car car;
+        static int i;
+        public Form1()
+        {
+            InitializeComponent();
+            car = new Car();
+            Car.Crash c = Crashed;
+            
+            car.OnCrash += c;
+        }
+
+        private void Crashed()
+        {
+            MessageBox.Show("Crashed!!!");
+            this.BackColor = Color.Red;
+        }
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+        private void foobar(object sender, System.EventArgs e)
+        {
+            car.SpeedUp();
+            this.Text = car.ToString();
+        }
+    }
+}
