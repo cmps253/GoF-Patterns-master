993d9c62d1e7519810a0994374b541af1f978600
Mahmoud Bdeir <mbdeir@aub.edu.lb>
2020-04-06 13:00:14 +0300

    Added Singleton, changed Prototype to Core 3.1

---
+++ Singleton/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Logger
+    {
+        private static Logger _logger;
+
+        private Logger(){}
+
+        public static Logger GetLogger()
+        {
+            if(_logger==null)
+            {
+                _logger = new Logger();
+            }
+            return _logger;
+        }
+        public void Log(string msg) => Console.WriteLine($"{DateTime.Now} {msg}");
+        private string fmt;
+        public void ChangeFormat(string fmt)
+        {
+            this.fmt = fmt;
+        }
+        public override string ToString()
+        {
+            return fmt;
+        }
+    }
+}
